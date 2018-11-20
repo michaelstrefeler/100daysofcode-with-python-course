8ca9186125d8de3347df71b9eca4cf2369bb0ab5
Strefeler Michael <mstrefel@intranet.epfl.ch>
2018-11-20 10:17:27 +0100

    Added missing bite from day 2

---
+++ days/01-03-datetimes/code/bite_67.py
@@ -0,0 +1,16 @@
+from datetime import date, timedelta
+
+start_100days = date(2017, 3, 30)
+pybites_founded = date(2016, 12, 19)
+pycon_date = date(2018, 5, 8)
+
+
+def get_hundred_days_end_date():
+    start_100days = date(2017, 3, 30)
+    return str(start_100days + timedelta(days=100))
+
+
+def get_days_between_pb_start_first_joint_pycon():
+    pybites_founded = date(2016, 12, 19)
+    pycon_date = date(2018, 5, 8)
+    return (pycon_date - pybites_founded).days
