d1056028f3199232239df9430811829350e295cd
Michael Strefeler <mstrefeler@me.com>
2018-11-23 10:52:51 +0100

    Day 30: Bite 2 Regex Fun

---
+++ days/28-30-regex/bite_2.py
@@ -0,0 +1,38 @@
+# bite_2.py
+# Regex Fun
+
+import re
+
+
+def extract_course_times():
+    """Write a regular expression that returns a list of timestamps:
+        ['01:47', '32:03', '41:51', '27:48', '05:02']"""
+    flask_course = ('Introduction 1 Lecture 01:47'
+                    'The Basics 4 Lectures 32:03'
+                    'Getting Technical!  4 Lectures 41:51'
+                    'Challenge 2 Lectures 27:48'
+                    'Afterword 1 Lecture 05:02')
+    return re.findall(r'\d{2}\:\d{2}', flask_course)
+
+
+def get_all_hashtags_and_links():
+    """Write a regular expression that returns this list:
+       ['http://pybit.es/requests-cache.html', '#python', '#APIs']"""
+    tweet = ('New PyBites article: Module of the Week - Requests-cache '
+             'for Repeated API Calls - http://pybit.es/requests-cache.html '
+             '#python #APIs')
+    return re.findall(r'((?:http|#)\S+)', tweet)
+
+
+def match_first_paragraph():
+    """Write a regular expression that returns  'pybites != greedy' """
+    html = ('<p>pybites != greedy</p>'
+            '<p>not the same can be said REgarding ...</p>')
+    return re.match(r'<p>(.*?)</p>', html).group(1)
+
+
+
+if __name__ == '__main__':
+    print(extract_course_times())
+    print(get_all_hashtags_and_links())
+    print(match_first_paragraph())
