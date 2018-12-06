7f7defe80754b3f520fcca8455986ead254a959e
michaelstrefeler <mstrefeler@me.com>
2018-12-06 08:53:09 +0100

    Day 43: Made skeleton app for API search

---
+++ days/43-45-search-api/search-other-api/api.py
@@ -0,0 +1,6 @@
+# import requests
+
+
+def init():
+    print('API initialised')
+    return 'It worked'
---
+++ days/43-45-search-api/search-other-api/program.py
@@ -0,0 +1,9 @@
+import api
+
+
+def main():
+    api.init()
+
+
+if __name__ == '__main__':
+    main()
