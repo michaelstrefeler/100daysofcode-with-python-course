e24cf48a45fc90a5c0e8d1a4b48c383e03c1fba4
Michael Strefeler <mstrefeler@me.com>
2018-12-17 13:19:03 +0100

    Day 53: Feedparser sanity check (error handling)

--- days/52-54-feedparser/my_code/parser.py
+++ days/52-54-feedparser/my_code/parser.py
@@ -4,5 +4,8 @@ FEED_FILE = 'newreleases.xml'
 
 feed = feedparser.parse(FEED_FILE)
 
-for entry in feed.entries:
-    print(f"{entry.published} - {entry.title}: {entry.link}") 
+if 'title' in feed.entries[0]:
+    for entry in feed.entries:
+        print(f"{entry.published} - {entry.title}: {entry.link}")
+else:
+    print("Tag not found.") 
