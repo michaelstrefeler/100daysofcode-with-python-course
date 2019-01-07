310d44a084804aa336f810937de21b3dc64cee89
michaelstrefeler <mstrefeler@me.com>
2019-01-07 09:14:56 +0100

    Day 71: fixed python example

--- days/70-72-openpyxl-excel-automation/code/excel_automation.py
+++ days/70-72-openpyxl-excel-automation/code/excel_automation.py
@@ -6,9 +6,10 @@ wb = load_workbook('Financial Sample.xlsx')
 ws1 = wb['Finances 2017']
 maxrow = ws1.max_row
 
+
 def insert_sum():
-    ws1['L' + str(maxrow)] = "=SUM(L2:L" + str(maxrow - 1) + ")"
-  
+    ws1['L' + str(maxrow)] = f"=SUM(L2:L{str(maxrow - 1)})"
+
 
 if __name__ == "__main__":
     insert_sum()
