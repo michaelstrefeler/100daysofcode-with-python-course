ad5b0e87801967f7c10df992513d33ef6d91ed54
michaelstrefeler <mstrefeler@me.com>
2019-01-10 09:56:15 +0100

    Add the ability to change the table name in the script

--- days/79-81-sqlite3/code/generateddb.py
+++ days/79-81-sqlite3/code/generateddb.py
@@ -19,9 +19,17 @@ def prompt_for_name():
     return name
 
 
+def get_table_name():
+    table_name = input("What would you liek to name your table?: ")
+    table_name.replace(' ', '_')
+    return table_name
+
+
 if __name__ == "__main__":
     name = prompt_for_name()
+    table_name = get_table_name()
     with create_db(name) as cursor:
-        cursor.execute("""CREATE TABLE test_table
+        cursor.execute(f"""CREATE TABLE {table_name}
     (col1 TEXT, col2 TEXT, col3 TEXT, col4 INT)""")
     print(f'{name}.db has been created')
+    print(f'The table {table_name} has also been created')
