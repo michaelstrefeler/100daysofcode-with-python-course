66e5b9e16d3ceb7d46323c5f30440a0cf16935a2
Michael Strefeler <mstrefeler@me.com>
2019-01-22 15:26:33 +0100

    Day 90: Fixed home inventory app

--- days/88-90-home-inventory-app/code/inventory.py
+++ days/88-90-home-inventory-app/code/inventory.py
@@ -6,15 +6,15 @@ from contextlib import contextmanager
 
 DB = "Inventory.db"
 
-#Script that creates the DB on first launch, otherwise is just a check.
+# Script that creates the DB on first launch, otherwise is just a check.
 def first_launch():
     try:
-        conn = sqlite3.connect(DB)
+        sqlite3.connect(DB)
     except:
         sys.exit('Error code X')
 
-        
-#generator to yield a db cursor and close the connection nicely.
+
+# generator to yield a db cursor and close the connection nicely.
 @contextmanager
 def access_db():
     try:
@@ -26,7 +26,7 @@ def access_db():
         conn.close()
 
 
-#A somewhat dirty menu. Improvement Point (make it a dict perhaps)
+# A somewhat dirty menu. Improvement Point (make it a dict perhaps)
 def main_menu():
     menu = {}
     menu['1'] = "Add Room."
@@ -34,38 +34,43 @@ def main_menu():
     menu['3'] = "View Inventory List."
     menu['4'] = "Total Value."
     menu['5'] = "Exit."
+
+    real_menu = {
+        '1': 'add_room()',
+        '2': 'add_inventory(check_input())',
+        '3': 'view_inventory(check_input())',
+        '4': 'calc_total()',
+        '5': 'sys.exit()'
+    }
     while True:
         print("\n")
         for item, desc in sorted(menu.items()):
             print(item, desc)
 
         choice = input("Selection: ")
-        if choice == '1':
-            add_room()
-        elif choice == '2':
-            add_inventory(check_input())
-        elif choice == '3':
-            view_inventory(check_input())
-        elif choice == '4':
-            calc_total()
-        elif choice == '5':
-            sys.exit()
-        else:
-            print("Invalid option, try again.")
+        try:
+            exec(real_menu[choice])
+        except KeyError:
+            print(f"'{choice}' is not a valid option. Try again.")
 
 
-#Adds a room. Scrubs the input first to only allow default chars.
+# Adds a room. Scrubs the input first to only allow default chars.
 def add_room():
     name = input("\nWhat name would you like to give the room? ")
     name = scrub(name)
-    with access_db() as cursor:
-        cursor.execute("CREATE TABLE '" + name.lower() + "' """"
-                                    (Item TEXT, Value REAL)
-                                    """)
-        print("\nA room with name %s has been added to the db.\n" % name)
+
+    if name.strip() == '':
+        print('Invalid choice')
+    else:
+        with access_db() as cursor:
+            cursor.execute("CREATE TABLE IF NOT EXISTS'" + name.lower() + "' "
+                           """
+                                        (Item TEXT, Value REAL)
+                                        """)
+            print("\nA room with name %s has been added to the db.\n" % name)
 
 
-#Parses the DB for table names and returns a list of the names.
+# Parses the DB for table names and returns a list of the names.
 def list_rooms():
     room_list = []
     with access_db() as cursor:
@@ -75,12 +80,12 @@ def list_rooms():
     return room_list
 
 
-#The scrubbing function. Removes all chars that aren't letters and numbers.
+# The scrubbing function. Removes all chars that aren't letters and numbers.
 def scrub(table_name):
-    return ''.join( chr for chr in table_name if chr.isalnum() )
+    return ''.join(chr for chr in table_name if chr.isalnum())
 
 
-#Checks the users' input to see if it matches a room/table name.
+# Checks the users' input to see if it matches a room/table name.
 def check_input():
     while True:
         print('\n')
@@ -93,11 +98,16 @@ def check_input():
             return scrub(selection)
 
 
-#Allows users to add an item and value to the DB of a specific room.
+# Allows users to add an item and value to the DB of a specific room.
 def add_inventory(selection):
     while True:
         name = input("\nName of item: ")
         cost = input("Monetary value: ")
+        try:
+            cost = float(cost)
+        except ValueError:
+            print('Invalid cost')
+            break
         with access_db() as cursor:
             cursor.execute("INSERT INTO '" + selection + "' VALUES(?, ?)", [name, cost])
 
@@ -106,7 +116,7 @@ def add_inventory(selection):
             break
 
 
-#Returns a list of all items in a room and their total value.
+# Returns a list of all items in a room and their total value.
 def view_inventory(selection):
     total = 0
     with access_db() as cursor:
@@ -118,7 +128,7 @@ def view_inventory(selection):
         print("Total Value: $%d" % total)
 
 
-#Function to calculate the $ total of the entire database.
+# Function to calculate the $ total of the entire database.
 def calc_total():
     total = 0
     room_list = list_rooms()
